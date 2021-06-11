ae544bc9fa621b07d02f7a6119b51110a09c36e9
Linloir <3145078758@qq.com>
2021-06-11 15:19:46 +0800

    适应头文件中的函数扩展

--- 0_Common_Arithmetic/Graph_In_C/Graph_In_C.c
+++ 0_Common_Arithmetic/Graph_In_C/Graph_In_C.c
@@ -5,8 +5,8 @@ int main(){
     Graph sample_graph = graph_init(1);
     graph_create(sample_graph);
     printf("DFS:\n");
-    graph_DFS_traverse(sample_graph, graph_visit);
+    graph_DFS_traverse(sample_graph, 0, graph_visit);
     printf("BFS:\n");
-    graph_BFS_traverse(sample_graph, graph_visit);
+    graph_BFS_traverse(sample_graph, 0, graph_visit);
     return 0;
 }
