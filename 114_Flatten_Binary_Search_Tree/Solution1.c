b08dbdca0928209e4c3ce7ab2b8d05631bb109a9
Linloir <3145078758@qq.com>
2022-01-29 11:40:04 +0800

    New Solutions

---
+++ 114_Flatten_Binary_Search_Tree/Solution1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../TestBench/common_tree.h"
+#include "../TestBench/linked_list.h"
+
+struct TreeNode* flattenTraverse(struct TreeNode* root){
+    struct TreeNode* last = root;
+    if(root->left){
+        struct TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        last = flattenTraverse(root->right);
+        last->right = temp;
+    }
+    return last->right ? flattenTraverse(last->right) : last;
+}
+
+void flatten(struct TreeNode* root){
+    flattenTraverse(root);
+}
+
+//TestBench
+void TestBench(){
+    Tree testTree = GenerateTree();
+    flatten(testTree);
+    PrintTree(testTree);
+}
+
+int main(){
+    TestBench();
+    return 0;
+}
