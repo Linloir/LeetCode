4cbc7e8bb0fbb1bbb4e69b5a247b141b2ccc900e
Linloir <3145078758@qq.com>
2022-02-09 14:25:27 +0800

    New Solutions

--- 2006_Count_Numbers_Of_Pairs_With_Difference_K/C_Solution_1.c
+++ 2006_Count_Numbers_Of_Pairs_With_Difference_K/C_Solution_1.c
@@ -1,6 +1,21 @@
 /*** 
  * Author       : Linloir
  * Date         : 2022-02-09 12:08:04
- * LastEditTime : 2022-02-09 12:08:04
- * Description  : 
+ * LastEditTime : 2022-02-09 14:25:21
+ * Description  : [9/2/2022 daily] | Leetcode 2006 | Easy | C Solution 1
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int countKDifference(int* nums, int numsSize, int k){
+    int count = 0;
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i + 1; j < numsSize; j++){
+            if(abs(nums[i] - nums[j]) == k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
